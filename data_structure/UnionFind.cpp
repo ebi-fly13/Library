410758a2ac42d3a3fc8380dcc7843eefb5f1a544
root <root@LAPTOP-2KQ7A2D6.localdomain>
2021-01-04 23:54:51 +0900

    add UnionFind.cpp

---
+++ data_structure/UnionFind.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+struct UnionFind{
+private:
+    std::vector<int> par;
+
+public:
+    UnionFind(int n=0): par(n,-1){ }
+
+    bool same(int x,int y){
+        return leader(x)==leader(y);
+    }
+
+    bool merge(int x, int y){
+        x = leader(x); y = leader(y);
+        if(x==y) return false;
+        if(par[x]>par[y]) std::swap(x,y);
+        par[x]+=par[y];
+        par[y] = x;
+        return true;
+    }
+
+    int leader(int x){
+        if(par[x]<0) return x;
+        else return par[x] = leader(par[x]);
+    }
+
+    int size(int x){
+        return -par[leader(x)];
+    }
+
+    int count_group(){
+        int c = 0;
+        for(int i = 0; i<par.size(); i++){
+            if(par[i]<0) c++; 
+        }
+        return c;
+    }
+};
+
+// verify "https://atcoder.jp/contests/practice2/tasks/practice2_a"
+int main(){
+    int n,q; std::cin >> n >> q;
+    UnionFind uf(n);
+    while(q--){
+        int t,u,v; std::cin >> t >> u >> v;
+        if(t==0){
+            uf.merge(u,v);
+        }
+        else{
+            if(uf.same(u,v)){
+                std::cout << "1\n";
+            }
+            else{
+                std::cout << "0\n";
+            }
+        }
+    }
+    return 0;
+}
