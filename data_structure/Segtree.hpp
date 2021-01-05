9487379f9c271de68c4c9a30686f1abdfc20a4d1
root <root@LAPTOP-2KQ7A2D6.localdomain>
2021-01-05 22:26:45 +0900

    add Segtree

---
+++ data_structure/Segtree.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+
+template<class Monoid, Monoid (*op)(Monoid, Monoid), Monoid (*e)()>
+struct Segtree {
+private:
+    std::vector<Monoid> data;
+    int n;
+public:
+    Segtree(int _n) : n(1) {
+        while(n<_n){
+            n <<= 1;
+        }
+        data.assign(2*n-1, e());
+    }
+
+    Segtree(std::vector<Monoid> v) : n(1) {
+        int _n = v.size();
+        while(n<_n){
+            n <<= 1;
+        }
+        data.assign(2*n-1, e());
+        for(int i = 0; i<_n; i++){
+            data[n+i-1] = v[i];
+        }
+        for(int i = n-2; i>=0; i--){
+            data[i] = op(data[2*i+1], data[2*i+2]);
+        }
+    }
+
+    void set(int p, Monoid x){
+        int k = n+p-1;
+        data[k] = x;
+        while(k>0){
+            k = (k-1)/2;
+            data[k] = op(data[2*k+1], data[2*k+2]);
+        }
+    }
+
+    Monoid prod(int tl, int tr, int l=0, int r=-1, int index=0){
+        if(r<0) r = n;
+        if(tr<=l || r<=tl){
+            return e();
+        }
+        if(tl<=l && r<=tr){
+            return data[index];
+        }
+        return op(prod(tl, tr, l, (l+r)/2, 2*index+1), prod(tl, tr, (l+r)/2, r, 2*index+2));
+    }
+
+    Monoid all_prod() { return data[0]; }
+
+    Monoid operator [] (int p) { return data[n+p-1]; }
+};
---
+++ test/Segtree.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include "../data_structure/Segtree.hpp"
+
+#include <iostream>
+#include <cstdint>
+
+using i64 = std::int64_t;
+
+i64 op(i64 a, i64 b) { return a+b; }
+i64 e() { return (i64)0; }
+
+int main(){
+    int n,q; std::cin >> n >> q;
+    std::vector<i64> a(n);
+    for(int i = 0; i<n; i++){
+        std::cin >> a[i];
+    }
+    Segtree<i64, op, e> seg(a);
+    while(q--){
+        int t;
+        std::cin >> t;
+        if(t==0){
+            int p; i64 x; std::cin >> p >> x;
+            seg.set(p,seg[p]+x);
+        }
+        else{
+            int l,r; std::cin >> l >> r;
+            std::cout << seg.prod(l,r) << std::endl;
+        }
+    }
+    return 0;
+}
