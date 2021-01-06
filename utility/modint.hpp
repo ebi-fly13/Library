d07b52dc2c012137c498d5878eb8fde5a1692a65
root <root@LAPTOP-2KQ7A2D6.localdomain>
2021-01-06 14:37:17 +0900

    add modint

---
+++ utility/modint.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+/*
+    author: noshi91
+    reference: https://noshi91.hatenablog.com/entry/2019/03/31/174006
+    noshi91のブログで公開しているmodintを元にinv(), pow()を追加したものです
+*/
+
+#include <cstdint>
+
+template<std::uint_fast64_t Modulus>
+class modint {
+  using u64 = std::uint_fast64_t;
+
+public:
+    u64 a;
+
+    constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
+    constexpr u64 &value() noexcept { return a; }
+    constexpr const u64 &value() const noexcept { return a; }
+    constexpr modint operator+(const modint rhs) const noexcept {
+        return modint(*this) += rhs;
+    }
+    constexpr modint operator-(const modint rhs) const noexcept {
+        return modint(*this) -= rhs;
+    }
+    constexpr modint operator*(const modint rhs) const noexcept {
+        return modint(*this) *= rhs;
+    }
+    constexpr modint operator/(const modint rhs) const noexcept {
+        return modint(*this) /= rhs;
+    }
+    constexpr modint &operator+=(const modint rhs) noexcept {
+        a += rhs.a;
+        if (a >= Modulus) {
+            a -= Modulus;
+        }
+        return *this;
+    }
+    constexpr modint &operator-=(const modint rhs) noexcept {
+        if (a < rhs.a) {
+        a += Modulus;
+        }
+        a -= rhs.a;
+        return *this;
+    }
+    constexpr modint &operator*=(const modint rhs) noexcept {
+        a = a * rhs.a % Modulus;
+        return *this;
+    }
+    constexpr modint &operator/=(modint rhs) noexcept {
+        u64 exp = Modulus - 2;
+        while (exp) {
+        if (exp % 2) {
+            *this *= rhs;
+        }
+        rhs *= rhs;
+        exp /= 2;
+        }
+        return *this;
+    }
+    modint pow(u64 n) const noexcept {
+        modint res = 1;
+        modint x = a;
+        while(n>0){
+            if(n&1) res *= x;
+            x *= x;
+            n >>=1;
+        }
+        return res;
+    }
+    modint inv() const {
+        return pow(Modulus-2);
+    }
+};
