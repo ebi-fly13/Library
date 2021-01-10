e0efd3fe554e05222b1acac20aba05abfa03faa7
root <root@LAPTOP-2KQ7A2D6.localdomain>
2021-01-10 17:52:38 +0900

    add LazySegtree

---
+++ data_structure/LazySegtree.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+/*
+    reference: https://beet-aizu.hatenablog.com/entry/2017/12/01/225955
+               https://atcoder.github.io/ac-library/master/document_ja/lazysegtree.html
+*/
+
+#include <vector>
+
+template<
+    class S,
+    S (*op)(S, S),
+    S (*e)(),
+    class F,
+    S (*mapping)(F, S),
+    F (*merge)(F, F),
+    F (*id)()>
+struct LazySegtree {
+private:
+    std::vector<S> data;
+    std::vector<F> lazy;
+    int n;
+
+    void eval(int index, int l, int r) {
+        data[index] = mapping(lazy[index], data[index]);
+        if(r-l>1) {
+            lazy[2*index+1] = merge(lazy[index] ,lazy[2*index+1]);
+            lazy[2*index+2] = merge(lazy[index] ,lazy[2*index+2]);
+        }
+        lazy[index] = id();
+    }
+
+public:
+    LazySegtree(int _n) : n(1) {
+        while(n<_n) {
+            n <<= 1;
+        }
+        data.assign(2*n-1, e());
+        lazy.assign(2*n-1, id());
+    }
+
+    LazySegtree(std::vector<S> v) : n(1) {
+        int _n = v.size();
+        while(n<_n) {
+            n <<= 1;
+        }
+        data.assign(2*n-1, e());
+        lazy.assign(2*n-1, id());
+        for(int i = 0; i<_n; i++) {
+            data[i+n-1] = v[i];
+        }
+        for(int i = n-2; i>=0; i--) {
+            data[i] = op(data[2*i+1], data[2*i+2]);
+        }
+    }
+
+    S prod(int l, int r, int nl = 0, int nr = -1, int index = 0) {
+        if(nr < 0) nr = n;
+        if(nr<=l || r<=nl) {
+            return e();
+        }
+        eval(index, nl, nr);
+        if(l<=nl && nr<=r) {
+            return data[index];
+        }
+        return op(prod(l, r, nl, (nl+nr)/2, 2*index+1), prod(l, r, (nl+nr)/2, nr, 2*index+2));
+    }
+
+    S all_prod() {
+        eval(0,0,n);
+        return data[0];
+    }
+
+    void apply(int l, int r, F f, int nl = 0, int nr = -1, int index = 0) {
+        if(nr<0) nr = n;
+        eval(index, nl, nr);
+        if(nr<=l || r<=nl) return;
+        if(l<=nl && nr<=r) {
+            lazy[index] = merge(f, lazy[index]);
+            eval(index, nl, nr);
+            return;
+        }
+        apply(l, r, f, nl, (nl+nr)/2, 2*index+1);
+        apply(l, r, f, (nl+nr)/2, nr, 2*index+2);
+        data[index] = op(data[2*index+1], data[2*index+2]);
+        return;
+    }
+};
---
+++ test/LazySegtree.test.cpp
@@ -0,0 +1,51 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include "../data_structure/LazySegtree.hpp"
+#include "../utility/modint.hpp"
+
+#include <iostream>
+#include <vector>
+
+using mint = modint998244353;
+
+struct S {
+    mint a;
+    int size;
+};
+
+struct F {
+    mint a, b;
+    F(mint a, mint b) : a(a), b(b) { }
+};
+
+S op(S l, S r) { return S{l.a + r.a, l.size + r.size}; }
+
+S e() { return S{0, 0}; }
+
+S mapping(F l, S r) { return S{r.a * l.a + (mint)r.size * l.b, r.size}; }
+
+F merge(F l, F r) { return F{r.a * l.a, r.b * l.a + l.b}; }
+
+F id() { return F{1, 0}; }
+
+int main(){
+    int n,q; std::cin >> n >> q;
+    std::vector<S> v(n);
+    for(int i = 0; i<n; i++) {
+        int a;
+        std::cin >> a;
+        v[i] = {a,1};
+    }
+    LazySegtree<S,op,e,F,mapping,merge,id> seg(v);
+    while(q--) {
+        int t; std::cin >> t;
+        if(t==0) {
+            int l,r,b,c; std::cin >> l >> r >> b >> c;
+            seg.apply(l,r,F(b,c));
+        }
+        else {
+            int l,r; std::cin >> l >> r;
+            std::cout << seg.prod(l,r).a.value() << std::endl;
+        }
+    }
+}
