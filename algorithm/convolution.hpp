86e9252dce904051488959bf9943da7717426535
root <root@LAPTOP-2KQ7A2D6.localdomain>
2021-01-18 01:00:35 +0900

    add convolution

---
+++ algorithm/convolution.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+/*
+    reference: https://hcpc-hokudai.github.io/archive/math_fft_002.pdf
+    mod 998244353 上での畳み込みを O(N log N) で求める.
+*/
+
+#include "../utility/modint.hpp"
+
+#include <vector>
+
+using mint = modint998244353;
+
+const mint primitive_root = 3;
+
+constexpr int mod = 998244353;
+
+void dft(std::vector<mint> &f) {
+    int n = f.size();
+    if(n==1) return;
+    std::vector<mint> a(n/2), b(n/2);
+    for(int i = 0; i<n/2; ++i) {
+        a[i] = f[2*i];
+        b[i] = f[2*i+1];
+    }
+    dft(a);
+    dft(b);
+    const mint zeta = primitive_root.pow((mod-1)/n);
+    mint x = 1;
+    for(int i = 0; i<n; ++i) {
+        f[i] = a[i%(n/2)] + x*b[i%(n/2)];
+        x *= zeta;
+    }
+}
+
+void inv_dft(std::vector<mint> &f) {
+    int n = f.size();
+    if(n==1) return;
+    std::vector<mint> a(n/2), b(n/2);
+    for(int i = 0; i<n/2; ++i) {
+        a[i] = f[2*i];
+        b[i] = f[2*i+1];
+    }
+    inv_dft(a);
+    inv_dft(b);
+    const mint inv_zeta = primitive_root.pow((mod-1)/n).inv();
+    mint x = 1;
+    for(int i = 0; i<n; ++i) {
+        f[i] = a[i%(n/2)] + x*b[i%(n/2)];
+        x *= inv_zeta;
+    }
+}
+
+std::vector<mint> convolution(const std::vector<mint> &f, const std::vector<mint> &g) {
+    int n = 1;
+    while(n < (int)f.size() + (int)g.size() - 1) n <<= 1;
+    std::vector<mint> a(n,0), b(n,0), fg(n,0);
+    std::copy(f.begin(), f.end(), a.begin());
+    std::copy(g.begin(), g.end(), b.begin());
+    dft(a);
+    dft(b);
+    for(int i = 0; i<n; ++i) {
+        fg[i] = a[i]*b[i];
+    }
+    inv_dft(fg);
+    for(int i = 0; i<n; ++i) {
+        fg[i] /= mint(n);
+    }
+    return fg;
+}
---
+++ test/convolution.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+
+#include "../algorithm/convolution.hpp"
+#include "../utility/modint.hpp"
+
+#include <iostream>
+
+using mint = modint998244353;
+
+int main() {
+    int n,m;
+    std::cin >> n >> m;
+    std::vector<mint> a(n),b(m);
+    for(int i = 0; i<n; ++i) {
+        std::cin >> a[i].a;
+    }
+    for(int i = 0; i<m; ++i) {
+        std::cin >> b[i].a;
+    }
+    auto c = convolution(a, b);
+    for(int i = 0; i<n+m-1; ++i) {
+        std::cout << c[i].value() << " ";
+    }
+    std::cout << '\n';
+}
