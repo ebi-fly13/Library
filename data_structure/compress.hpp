38975ddd27e0c3beef3408e31fffc3b29bc61b49
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2021-07-15 23:14:48 +0900

    add compress

---
+++ data_structure/compress.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+
+namespace ebi {
+
+template<class T>
+struct compress {
+private:
+    std::vector<T> cp;
+    bool flag = false;
+public:
+    compress() = default;
+
+    compress(std::vector<T> cp) : cp(cp) {
+        build();
+    }
+
+    void build() {
+        std::sort(cp.begin(), cp.end());
+        cp.erase(std::unique(cp.begin(), cp.end()), cp.end());
+        flag = true;
+    }
+
+    void add(const T &val) {
+        cp.emplace_back(val);
+        flag = false;
+    }
+
+    int get(const T &val) {
+        if(flag == false) build();
+        return std::lower_bound(cp.begin(), cp.end(), val) - cp.begin();
+    }
+
+    int size() const {
+        return cp.size();
+    }
+};
+
+}
