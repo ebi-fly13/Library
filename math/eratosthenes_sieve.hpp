2336e056c71521a87a3ea0231a3c558f36f4bf71
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2021-08-16 17:42:28 +0900

    update eratosthenes_sieve

--- math/eratosthenes_sieve.hpp
+++ math/eratosthenes_sieve.hpp
@@ -28,6 +28,34 @@ public:
         return table[p];
     }
 
+    template<class T>
+    std::vector<T> fast_zeta(const std::vector<T> &f) {
+        std::vector<T> F = f;
+        int sz = f.size();
+        assert(sz <= n+1);
+        for(int i = 2; i < sz; i++) {
+            if(!table[i]) continue;
+            for(int j = (sz-1)/i; j >= 1; j--) {
+                F[j] += F[j * i];
+            }
+        }
+        return F;
+    }
+
+    template<class T>
+    std::vector<T> fast_mobius(const std::vector<T> &F) {
+        std::vector<T> f = F;
+        int sz = F.size();
+        assert(sz <= n+1);
+        for(int i = 2; i < sz; i++) {
+            if(!table[i]) continue;
+            for(int j = 1; j*i < sz; j++) {
+                f[j] -= f[j * i];
+            }
+        }
+        return f;
+    }
+
     std::vector<int> prime_table(int m = -1) {
         if(m < 0) m = n;
         std::vector<int> prime;
