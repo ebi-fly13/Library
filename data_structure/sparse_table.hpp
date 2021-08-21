ab82097d3e8a620423c58119f5299b22fdd7e4a7
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2021-08-21 15:49:26 +0900

    add sparse_table

---
+++ data_structure/sparse_table.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+
+/*
+    reference: https://scrapbox.io/data-structures/Sparse_Table
+*/
+
+namespace ebi {
+
+template<class Band, Band (*op)(Band, Band)>
+struct sparse_table {
+public:
+    sparse_table() = default;
+
+    sparse_table(const std::vector<Band> &a) : n(a.size()) {
+        table = std::vector(std::__lg(n) + 1, std::vector<Band>(n));
+        for(int i = 0; i < n; i++) {
+            table[0][i] = a[i];
+        }
+        for(int k = 1; (1<<k) <= n; k++) {
+            for(int i = 0; i < n; i++) {
+                table[k][i] = op(table[k-1][i], table[k-1][i + (1<<(k-1))]);
+            }
+        }
+    }
+
+    // [l, r)
+    Band fold(int l, int r) {
+        int k = std::__lg(r-l);
+        return op(table[k][l], table[k][r-(1<<k)]);
+    }
+private:
+    int n;
+    std::vector<std::vector<Band>> table;
+};
+
+}
---
+++ test/static_rmq.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <iostream>
+#include <vector>
+
+#include "../data_structure/sparse_table.hpp"
+
+int op(int a, int b) {
+    return a < b ? a : b;
+}
+
+int main() {
+    int n,q;
+    std::cin >> n >> q;
+    std::vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    ebi::sparse_table<int, op> st(a);
+    while(q--) {
+        int l,r;
+        std::cin >> l >> r;
+        std::cout << st.fold(l, r) << std::endl;
+    }
+}
