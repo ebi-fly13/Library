0399bdf679d79f506a151b5d4ef4d0094ffb2518
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2021-09-15 01:03:02 +0900

    fix

--- geometry/point.hpp
+++ geometry/point.hpp
@@ -137,7 +137,7 @@ long double area(const std::vector<point> &p) {
     long double s = 0;
     int n = p.size();
     for(int i = 0; i < n; i++) {
-        s = internal::add(s, det(p[i], p[(i+1 == n) ? 0 : i+1]));
+        s = internal::add(s, det(p[i], p[(i+1 != n) ? i+1 : 0]));
     }
     s /= 2.0;
     return s;
