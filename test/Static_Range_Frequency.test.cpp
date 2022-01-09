4765d97ed7886b5f6d6a572bf15d08dd82697238
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2022-01-10 06:39:24 +0900

    add test about wevelet matrix

---
+++ test/Static_Range_Frequency.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_frequency"
+
+
+#include <vector>
+#include <cstdint>
+#include <iostream>
+
+#include "data_structure/WaveletMatrix.hpp"
+
+int main() {
+    int n,q;
+    std::cin >> n >> q;
+    std::vector<int> a(n);
+    for(auto &val: a) {
+        std::cin >> val;
+    }
+    ebi::WaveletMatrix<int> wm(a);
+    while(q--) {
+        int l,r,x;
+        std::cin >> l >> r >> x;
+        if(l == r) {
+            std::cout << "0\n";
+            continue;
+        }
+        std::cout << wm.rank(r, x) - wm.rank(l, x) << '\n';
+    }
+    return 0;
+}
