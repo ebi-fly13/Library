8b9d8c44f0014c3d25c5e5a3dba8856c628bc6e0
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-04-22 18:39:10 +0900

    update hld

--- test/vertex_add_path_sum.test.cpp
+++ test/vertex_add_path_sum.test.cpp
@@ -1,6 +1,8 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/vertex_add_path_sum"
 
-#include "../data_structure/heavy_light_decomposition.hpp"
+#include "../data_structure/segtree.hpp"
+#include "../tree/heavy_light_decomposition.hpp"
+#include "../graph/template.hpp"
 
 #include <iostream>
 #include <vector>
@@ -16,14 +18,26 @@ int main() {
     for(int i = 0; i < n; ++i) {
         std::cin >> a[i];
     }
-    ebi::heavy_light_decomposition<i64, op, e> hld(n);
+    ebi::graph g(n);
     for(int i = 0; i < n-1; ++i) {
         int u,v;
         std::cin >> u >> v;
-        hld.add_edge(u,v);
+        g.add_edge(u,v);
     }
-    hld.build();
-    hld.set(a);
+    ebi::heavy_light_decomposition hld(g);
+    ebi::segtree<i64, op, e> seg(n);
+    i64 ans = e();
+    auto set = [&](int u, i64 x) {
+        int idx = hld.idx(u);
+        seg.set(idx, seg.get(idx) + x);
+    };
+    auto f = [&](int l, int r) {
+        if(l <= r) ans = op(ans, seg.prod(l, r));
+        else ans = op(ans, seg.prod(r, l));
+    };
+    for(int i = 0; i < n; i++) {
+        set(i, a[i]);
+    }
     while(q--) {
         int flag;
         std::cin >> flag;
@@ -31,12 +45,14 @@ int main() {
             int p;
             i64 x;
             std::cin >> p >> x;
-            hld.set(p, hld.get(p) + x);
+            set(p, x);
         }
         else {
             int u,v;
             std::cin >> u >> v;
-            std::cout << hld.path_prod(u, v) << std::endl;
+            ans = e();
+            hld.path_noncommutative_query(u, v, f);
+            std::cout << ans << '\n';
         }   
     }
 }
---
+++ tree/heavy_light_decomposition.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace ebi {
+
+struct heavy_light_decomposition {
+   private:
+    void dfs_sz(int v) {
+        for (auto &nv : g[v]) {
+            if (nv == par[v]) continue;
+            par[nv] = v;
+            depth[nv] = depth[v] + 1;
+            dfs_sz(nv);
+            sz[v] += sz[nv];
+            if (sz[nv] > sz[g[v][0]] || g[v][0] == par[v])
+                std::swap(nv, g[v][0]);
+        }
+    }
+
+    void dfs_hld(int v) {
+        static int t = 0;
+        in[v] = t++;
+        for (auto nv : g[v]) {
+            if (nv == par[v]) continue;
+            nxt[nv] = (nv == g[v][0] ? nxt[v] : nv);
+            dfs_hld(nv);
+        }
+        out[v] = t;
+    }
+
+    // [u, v) パスの取得 (v は u の祖先)
+    std::vector<std::pair<int, int>> ascend(int u, int v) const {
+        std::vector<std::pair<int, int>> res;
+        while (nxt[u] != nxt[v]) {
+            res.emplace_back(in[u], in[nxt[u]]);
+            u = par[nxt[u]];
+        }
+        if (u != v) res.emplace_back(in[u], in[v] + 1);
+        return res;
+    }
+
+    // (u, v] パスの取得 (u は v の祖先)
+    std::vector<std::pair<int, int>> descend(int u, int v) const {
+        if (u == v) return {};
+        if (nxt[u] == nxt[v]) return {{in[u] + 1, in[v]}};
+        auto res = descend(u, par[nxt[v]]);
+        res.emplace_back(in[nxt[v]], in[v]);
+        return res;
+    }
+
+   public:
+    heavy_light_decomposition(const std::vector<std::vector<int>> &gh,
+                              int root = 0)
+        : n(gh.size()),
+          g(gh),
+          sz(n, 1),
+          in(n),
+          out(n),
+          nxt(n),
+          par(n, -1),
+          depth(n, 0) {
+        dfs_sz(root);
+        dfs_hld(root);
+    }
+
+    int idx(int u) const { return in[u]; }
+
+    int lca(int u, int v) const {
+        while (nxt[u] != nxt[v]) {
+            if (in[u] < in[v]) std::swap(u, v);
+            u = par[nxt[u]];
+        }
+        return depth[u] < depth[v] ? u : v;
+    }
+
+    int distance(int u, int v) const {
+        return depth[u] + depth[v] - 2 * depth[lca(u, v)];
+    }
+
+    template <class F>
+    void path_noncommutative_query(int u, int v, const F &f) const {
+        int l = lca(u, v);
+        for (auto [a, b] : ascend(u, l)) f(a + 1, b);
+        f(in[l], in[l] + 1);
+        for (auto [a, b] : descend(l, v)) f(a, b + 1);
+    }
+
+    template <class F>
+    void subtree_query(int u, bool vertex, const F &f) {
+        f(in[u] + int(!vertex), out[u]);
+    }
+
+   private:
+    int n;
+    std::vector<std::vector<int>> g;
+    std::vector<int> sz, in, out, nxt, par, depth;
+};
+
+}  // namespace ebi
