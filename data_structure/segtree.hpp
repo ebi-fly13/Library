bb366099babc0c8ebac454760699539d1520e2c7
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-04-23 15:36:52 +0900

    fix segtree

--- data_structure/segtree.hpp
+++ data_structure/segtree.hpp
@@ -5,69 +5,106 @@
 
 namespace ebi {
 
-template<class Monoid, Monoid (*op)(Monoid, Monoid), Monoid (*e)()>
+template <class S, S (*op)(S, S), S (*e)()>
 struct segtree {
-private:
+   private:
     int n;
-    int size;
-    std::vector<Monoid> data;
-public:
-    segtree(int _n) : n(_n), size(1) {
-        while(size < _n) {
-            size <<= 1;
-        }
-        data.assign(2*size, e());
-        return;
+    int sz;
+    std::vector<S> data;
+
+    void update(int i) { data[i] = op(data[2 * i], data[2 * i + 1]); }
+
+   public:
+    segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    segtree(const std::vector<S> &v) : n((int)v.size()), sz(1) {
+        while (sz < n) sz *= 2;
+        data = std::vector<S>(2 * sz, e());
+        for(int i = 0; i < n; i++) { data[sz + i] = v[i]; }
+        for(int i = sz-1; i >= 1; i--) update(i);
     }
 
-    segtree(const std::vector<Monoid> &v) : size(1) {
-        n = (int)v.size();
-        while(size < n) {
-            size <<= 1;
-        }
-        data.assign(2*size, e());
-        std::copy(v.begin(), v.end(), data.begin() + size);
-        for(int i = size-1; i > 0; i--) {
-            data[i] = op(data[i<<1|0], data[i<<1|1]);
-        }
-        return;
-    }
-
-    void set(int p, Monoid x) {
+    void set(int p, S x) {
         assert(0 <= p && p < n);
-        p += size;
+        p += sz;
         data[p] = x;
-        while(p > 1) {
+        while (p > 1) {
             p >>= 1;
-            data[p] = op(data[p<<1|0], data[p<<1|1]);
+            update(p);
         }
-        return;
     }
 
-    Monoid get(int p) const {
+    S get(int p) {
         assert(0 <= p && p < n);
-        return data[p+size];
+        return data[p + sz];
     }
 
-    Monoid prod(int l, int r) const {
+    S prod(int l, int r) {
         assert(0 <= l && l <= r && r <= n);
-        Monoid left = e(), right = e();
-        l += size;
-        r += size;
-        while(l < r) {
-            if(l & 1) left = op(left, data[l++]);
-            if(r & 1) right = op(data[--r], right);
+        S sml = e(), smr = e();
+        l += sz;
+        r += sz;
+        while (l < r) {
+            if (l & 1) sml = op(sml, data[l++]);
+            if (r & 1) smr = op(data[--r], smr);
             l >>= 1;
             r >>= 1;
         }
-        return op(left, right);
+        return op(sml, smr);
     }
 
-    Monoid all_prod() const {
-        return data[1];
+    S all_prod() { return data[1]; }
+
+    template <class F>
+    int max_right(int l, F f) {
+        assert(0 <= l && l < n);
+        assert(f(e()));
+        if (l == n) return n;
+        l += sz;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, data[l]))) {
+                while (l < sz) {
+                    l = 2 * l;
+                    if (f(op(sm, data[l]))) {
+                        sm = op(sm, data[l]);
+                        l++;
+                    }
+                }
+                return l - sz;
+            }
+            sm = op(sm, data[l]);
+            l++;
+        } while ((l & -l) != l);
+        return n;
     }
 
-    Monoid operator [] (int p) const { return data[size+p]; }
+    template <class F>
+    int min_left(int r, F f) {
+        assert(0 <= r && r <= n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += sz;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(data[r], sm))) {
+                while (r < sz) {
+                    r = 2 * r + 1;
+                    if (f(op(data[r], sm))) {
+                        sm = op(data[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - sz;
+            }
+            sm = op(data[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+    S operator [] (int p) const { return data[sz+p]; }
 };
 
 } // namespace ebi
--- docs/Segtree.md
+++ docs/Segtree.md
@@ -1,23 +1,23 @@
 ---
-title: Segtree
+title: segtree
 documentation_of: //data_structure/segtree.hpp
 ---
 
 ## 説明
 
-モノイドの列$(a_0,a_1,\dots,a_{n-1})$に対して各種操作ができるデータ構造である.
+モノイドの列$(a_0,a_1,\dots,a_{n-1})$に対して各種操作ができるデータ構造である。
 
 ## コンストラクタ
 
 ```cpp
-(1) Segtree<Monoid, op, e> seg(int n)
-(2) Segtree<Monoid, op, e> seg(std::vector<Monoid> v)
+(1) Segtree<S, op, e> seg(int n)
+(2) Segtree<S, op, e> seg(std::vector<S> v)
 ```
--   型 ```Monoid```
--   二項演算 ```Monoid op(Monoid, Monoid)```
--   単位元 ```Monoid e()```
+-   型 ```S```
+-   二項演算 ```S op(S, S)```
+-   単位元 ```S e()```
 
-を定義する必要がある.例としてRange Sum Queryならば以下のようになる.
+を定義する必要がある.例としてRange Sum Queryならば以下のようになる。
 
 ```cpp
 int op(int a, int b) { 
@@ -28,21 +28,24 @@ int e() {
     return 0; 
 }
 
-Segtree<int, op, e> seg(n);
+segtree<int, op, e> seg(n);
 ```
 
 計算量$O(n)$
 
 ## Operator
--   ```seg.set(int p, Monoid x)```
-    -   $a_p$に$x$を代入する.
+-   ```seg.get(int p)```
+    -   a[p]を返す
+-   ```seg.set(int p)```
+    -   $a_p$に$x$を代入する
     -   時間計算量$O(\log n)$
 -   ```seg.prod(int l, int r)```
-    -   ```op(a[l], ..., a[r-1])```を計算する.
+    -   ```op(a[l], ..., a[r-1])```を計算する
     -   時間計算量$O(\log n)$
 -   ```seg.all_prod()```
-    -   ```op(a[0], ..., a[n-1])```を計算する.
+    -   ```op(a[0], ..., a[n-1])```を計算する
     -   時間計算量$O(1)$
--   ```seg[int p]```
-    -   ```a[p]```を返す.
-    -   時間計算量$O(1)$
+-   ```seg.max_right(int l, F f)```
+    -   セグ木上の二分探索。$[l, r)$ が f を満たすような最大の $r$ を返す。
+-   ```seg.min_left(int r, F f)```
+    - セグ木上の二分探索。$[l, r)$ が f を満たすような最小の $l$ を返す。
