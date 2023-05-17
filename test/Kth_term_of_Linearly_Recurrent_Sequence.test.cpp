87a5a1b84eaf90ab4857ba34c28eeca855ccd964
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-05-17 18:28:16 +0900

    add test about bm

---
+++ test/Kth_term_of_Linearly_Recurrent_Sequence.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM \
+    "https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence"
+
+#include <iostream>
+#include <vector>
+
+#include "convolution/ntt.hpp"
+#include "math/boastan_mori_algorithm.hpp"
+#include "utility/modint.hpp"
+
+using mint = ebi::modint998244353;
+
+int main() {
+    int d;
+    long long k;
+    std::cin >> d >> k;
+    std::vector<mint> a(d), c(d);
+    for (int i = 0; i < d; i++) {
+        int val;
+        std::cin >> val;
+        a[i] = val;
+    }
+    for (int i = 0; i < d; i++) {
+        int val;
+        std::cin >> val;
+        c[i] = val;
+    }
+    std::cout << ebi::kitamasa<mint, ebi::convolution>(k, a, c).val() << '\n';
+}
