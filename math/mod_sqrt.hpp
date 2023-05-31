b6a912dbfa762393aac0776984aa2e73115029cd
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-05-31 10:21:15 +0900

    fix mod_sqrt

--- math/mod_sqrt.hpp
+++ math/mod_sqrt.hpp
@@ -1,26 +1,28 @@
 #pragma once
 
+#include <cstdint>
 #include <optional>
 
 #include "../utility/dynamic_modint.hpp"
 
 namespace ebi {
 
-std::optional<long long> mod_sqrt(const long long &a, const long long &p) {
+std::optional<std::int64_t> mod_sqrt(const std::int64_t &a,
+                                     const std::int64_t &p) {
     if (a == 0 || a == 1) return a;
     using mint = dynamic_modint<100>;
     mint::set_mod(p);
     if (mint(a).pow((p - 1) >> 1) != 1) return std::nullopt;
     mint b = 1;
     while (b.pow((p - 1) >> 1) == 1) b += 1;
-    long long m = p - 1, e = 0;
+    std::int64_t m = p - 1, e = 0;
     while (m % 2 == 0) m >>= 1, e++;
     mint x = mint(a).pow((m - 1) >> 1);
     mint y = mint(a) * x * x;
     x *= a;
     mint z = b.pow(m);
     while (y != 1) {
-        long long j = 0;
+        std::int64_t j = 0;
         mint t = y;
         while (t != 1) {
             j++;
