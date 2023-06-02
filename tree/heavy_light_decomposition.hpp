a6dffdee2d5877bb4ddef392d2ad1bc70851d11b
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-06-03 02:01:06 +0900

    add jump on hld

--- tree/heavy_light_decomposition.hpp
+++ tree/heavy_light_decomposition.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -22,6 +23,7 @@ struct heavy_light_decomposition {
     void dfs_hld(int v) {
         static int t = 0;
         in[v] = t++;
+        rev[in[v]] = v;
         for (auto nv : g[v]) {
             if (nv == par[v]) continue;
             nxt[nv] = (nv == g[v][0] ? nxt[v] : nv);
@@ -60,7 +62,8 @@ struct heavy_light_decomposition {
           out(n),
           nxt(n),
           par(n, -1),
-          depth(n, 0) {
+          depth(n, 0),
+          rev(n) {
         dfs_sz(root);
         dfs_hld(root);
     }
@@ -69,12 +72,42 @@ struct heavy_light_decomposition {
         return in[u];
     }
 
+    int la(int v, int k) const {
+        while (1) {
+            int u = nxt[v];
+            if (in[u] <= in[v] - k) return rev[in[v] - k];
+            k -= in[v] - in[u] + 1;
+            v = par[u];
+        }
+    }
+
     int lca(int u, int v) const {
         while (nxt[u] != nxt[v]) {
             if (in[u] < in[v]) std::swap(u, v);
             u = par[nxt[u]];
         }
         return depth[u] < depth[v] ? u : v;
+    }
+
+    int jump(int s, int t, int i) const {
+        if (i == 0) return s;
+        int l = lca(s, t);
+        int d = depth[s] + depth[t] - depth[l] * 2;
+        if (d < i) return -1;
+        if (depth[s] - depth[l] >= i) return la(s, i);
+        i = d - i;
+        return la(t, i);
+    }
+
+    std::vector<int> path(int s, int t) const {
+        int l = lca(s, t);
+        std::vector<int> a, b;
+        for (; s != l; s = par[s]) a.emplace_back(s);
+        for (; t != l; t = par[t]) b.emplace_back(t);
+        a.emplace_back(l);
+        std::reverse(b.begin(), b.end());
+        a.insert(a.end(), b.begin(), b.end());
+        return a;
     }
 
     int distance(int u, int v) const {
@@ -97,7 +130,7 @@ struct heavy_light_decomposition {
   private:
     int n;
     std::vector<std::vector<int>> g;
-    std::vector<int> sz, in, out, nxt, par, depth;
+    std::vector<int> sz, in, out, nxt, par, depth, rev;
 };
 
 }  // namespace ebi
