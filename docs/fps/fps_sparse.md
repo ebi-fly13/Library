0aa62588ee714fc87b44b1f09145698adcc99ca8
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-06-07 21:51:08 +0900

    add pow of fps sparse

--- docs/fps/fps_sparse.md
+++ docs/fps/fps_sparse.md
@@ -1,8 +1,12 @@
 ---
-title: $Formal Power Series (Sparse)$
+title: Formal Power Series (Sparse)
 documentation_of: //fps/fps_sparse.hpp
 ---
 
 ## inv(std::vector<mint> f, int d)
 
 $f^{-1} \mod x^d$ を求める。$f$ の非ゼロの項を $M$ 個として $O(NM)$
+
+## pow(std::vector<mint> f, long long k, int d)
+
+$f^k \mod x^d$ を求める。$f$ の非ゼロの項を $M$ 個として $O(NM)$
--- fps/fps_sparse.hpp
+++ fps/fps_sparse.hpp
@@ -29,4 +29,57 @@ std::vector<mint> inv_sparse(const std::vector<mint> &f, int d = -1) {
     return g;
 }
 
+template <class mint>
+std::vector<mint> pow_sparse_1(const std::vector<mint> &f, long long k,
+                               int d = -1) {
+    int n = f.size();
+    assert(n == 0 || f[0] == 1);
+    std::vector<std::pair<int, mint>> ret;
+    for (int i = 1; i < n; i++) {
+        if (f[i] != 0) ret.emplace_back(i, f[i]);
+    }
+    std::vector<mint> g(d);
+    g[0] = 1;
+    for (int i = 0; i < d - 1; i++) {
+        for (auto [d, cf] : ret) {
+            if (i + 1 - d < 0) break;
+            g[i + 1] +=
+                (mint(k) * mint(d) - mint(i - d + 1)) * cf * g[i + 1 - d];
+        }
+        g[i + 1] /= i + 1;
+    }
+    return g;
+}
+
+template <class mint>
+std::vector<mint> pow_sparse(const std::vector<mint> &f, long long k,
+                             int d = -1) {
+    int n = f.size();
+    if (d < 0) d = n;
+    if (k == 0) {
+        std::vector<mint> g(d);
+        if (d > 0) g[0] = 1;
+        return g;
+    }
+    for (int i = 0; i < n; i++) {
+        if (f[i] != 0) {
+            mint rev = f[i].inv();
+            std::vector<mint> f2(n - i);
+            for (int j = i; j < n; j++) {
+                f2[j - i] = f[j] * rev;
+            }
+            f2 = pow_sparse_1(f2, k, d);
+            mint fk = f[i].pow(k);
+            std::vector<mint> g(d);
+            for (int j = 0; j < int(f2.size()); j++) {
+                if (j + i * k >= d) break;
+                g[j + i * k] = f2[j] * fk;
+            }
+            return g;
+        }
+        if (i >= (d + k - 1) / k) break;
+    }
+    return std::vector<mint>(d);
+}
+
 }  // namespace ebi
