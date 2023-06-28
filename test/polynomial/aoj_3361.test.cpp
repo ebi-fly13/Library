98044236dcfc02ff8379b1f46f97f53e528fd338
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-06-28 14:55:41 +0900

    add test for fps_sparse

---
+++ test/polynomial/aoj_3361.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3361"
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include "../../fps/fps_sparse.hpp"
+#include "../../math/combination.hpp"
+#include "../../utility/modint.hpp"
+
+using mint = ebi::modint998244353;
+ebi::combination<mint> comb(1000000);
+
+int main() {
+    int k, n;
+    std::vector<mint> f(300);
+    f[1] = 1;
+    f[256] = -256;
+    f[257] = 255;
+    while (std::cin >> k >> n, !(n == 0 && k == 0)) {
+        auto dp = ebi::pow_sparse(f, k, n + 1);
+        assert(int(dp.size()) == n + 1);
+        mint ans = 0;
+        for (int i = n; i >= 0; i--) {
+            ans += dp[i] * comb(2 * k - 1 + n - i, n - i);
+        }
+        std::cout << ans.val() << '\n';
+    }
+}
