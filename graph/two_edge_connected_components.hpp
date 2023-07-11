f81abf46c5ed81a74c80e4da4a7c3c25c177997d
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-07-11 14:52:21 +0900

    update

--- graph/two_edge_connected_components.hpp
+++ graph/two_edge_connected_components.hpp
@@ -8,24 +8,24 @@ struct two_edge_connected_components : low_link {
   private:
     void dfs(int v, int par = -1) {
         if (par != -1 && this->ord[par] >= this->low[v])
-            id[v] = id[par];
+            _id[v] = _id[par];
         else
-            id[v] = k++;
+            _id[v] = k++;
         for (auto nv : this->g[v]) {
-            if (id[nv] == -1) dfs(nv, v);
+            if (_id[nv] == -1) dfs(nv, v);
         }
     }
 
   public:
     two_edge_connected_components(const std::vector<std::vector<int>> &_g)
-        : low_link(_g), id(this->n, -1) {
+        : low_link(_g), _id(this->n, -1) {
         for (int i = 0; i < this->n; i++) {
-            if (id[i] == -1) dfs(i);
+            if (_id[i] == -1) dfs(i);
         }
         t.resize(k);
         for (auto [u, v] : this->_bridge) {
-            u = id[u];
-            v = id[v];
+            u = _id[u];
+            v = _id[v];
             t[u].emplace_back(v);
             t[v].emplace_back(u);
         }
@@ -38,14 +38,18 @@ struct two_edge_connected_components : low_link {
     std::vector<std::vector<int>> groups() const {
         std::vector _groups(k, std::vector<int>());
         for (int i = 0; i < n; i++) {
-            _groups[id[i]].emplace_back(i);
+            _groups[_id[i]].emplace_back(i);
         }
         return _groups;
     }
 
+    int id(int v) const {
+        return _id[v];
+    }
+
   private:
     int k = 0;
-    std::vector<int> id;
+    std::vector<int> _id;
     std::vector<std::vector<int>> t;
 };
 
