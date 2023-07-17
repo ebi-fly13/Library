1f2b381efb2638c056e6eb4be108c721bd230a9b
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-07-17 13:16:33 +0900

    fix

--- math/internal_math.hpp
+++ math/internal_math.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 namespace ebi {
 
 namespace internal {
@@ -10,6 +12,8 @@ constexpr int primitive_root_constexpr(int m) {
     if (m == 469762049) return 3;
     if (m == 754974721) return 11;
     if (m == 998244353) return 3;
+    assert(0);
+    return -1;
 }
 template <int m> constexpr int primitive_root = primitive_root_constexpr(m);
 
