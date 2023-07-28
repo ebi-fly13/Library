670a6fbfbb3c4fd8528e8e41f65b127d4a29993f
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-07-29 00:20:07 +0900

    add test

---
+++ test/math/#_p_subset_sum.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sharp_p_subset_sum"
+
+#include <iostream>
+#include <vector>
+
+#include "../../convolution/ntt.hpp"
+#include "../../fps/product_of_one_plus_xn.hpp"
+#include "../../utility/modint.hpp"
+
+using mint = ebi::modint998244353;
+
+int main() {
+    int n, t;
+    std::cin >> n >> t;
+    std::vector<int> s(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> s[i];
+    }
+    auto p = ebi::product_of_one_plus_xn<mint, ebi::convolution>(s, t + 1);
+    for (int i = 1; i <= t; i++) {
+        std::cout << p[i] << " \n"[i == t];
+    }
+}
