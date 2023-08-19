1978bdf0b3adb0e8ef494bc227eef9be69fcf9e3
Hiroto Yano <yano.h.ag@m.titech.ac.jp>
2023-08-19 23:22:23 +0900

    Update utility.hpp

--- template/utility.hpp
+++ template/utility.hpp
@@ -23,6 +23,24 @@ template <class T> inline bool chmax(T &a, T b) {
     return false;
 }
 
+template <class T> T safe_ceil(T a, T b) {
+    if (a % b == 0)
+        return a / b;
+    else if (a >= 0)
+        return (a / b) + 1;
+    else
+        return -((-a) / b);
+}
+
+template <class T> T safe_floor(T a, T b) {
+    if (a % b == 0)
+        return a / b;
+    else if (a >= 0)
+        return a / b;
+    else
+        return -((-a) / b) - 1;
+}
+
 constexpr i64 LNF = std::numeric_limits<i64>::max() / 4;
 
 constexpr int INF = std::numeric_limits<int>::max() / 2;
