8885d11b76da4a881c991e34cb40cea9d2599e89
Hiroto Yano <yano.h.ag@m.titech.ac.jp>
2023-08-31 02:52:44 +0900

    Update internal_math.hpp

--- math/internal_math.hpp
+++ math/internal_math.hpp
@@ -13,6 +13,7 @@ constexpr int primitive_root_constexpr(int m) {
     if (m == 754974721) return 11;
     if (m == 998244353) return 3;
     if (m == 880803841) return 26;
+    if (m == 924844033) return 5;
     return -1;
 }
 template <int m> constexpr int primitive_root = primitive_root_constexpr(m);
