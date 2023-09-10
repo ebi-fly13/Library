f9722276d4ce54f9ab6395cc59b1f43f5a86d66a
Hiroto Yano <yano.h.ag@m.titech.ac.jp>
2023-09-10 16:47:54 +0900

    add bfs on complement graph

---
+++ docs/graph/complement_graph_bfs.md
@@ -0,0 +1,14 @@
+---
+title: Complement Graph BFS
+documentation_of: //graph/complement_graph_bfs.hpp
+---
+
+## 説明
+
+頂点 $s$ から始めて、 $h$ の補グラフをBFSする。頂点数を $N$ 、辺の本数を $M$ 、`func`の計算量を $F$ として計算量は $O(NF + M)$ である。
+
+インターフェースは以下である。
+
+```
+void complement_graph_bfs(int s, std::vector<std::vector<int>> h, F func)
+```
---
+++ graph/complement_graph_bfs.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <list>
+#include <numeric>
+#include <queue>
+#include <vector>
+
+namespace ebi {
+
+template <class F>
+void complement_graph_bfs(int s, const std::vector<std::vector<int>> &h,
+                          const F &func) {
+    const int n = h.size();
+    std::vector<int> not_visit;
+    not_visit.reserve(n - 1);
+    for (int i = 0; i < n; i++)
+        if (i != s) not_visit.emplace_back(i);
+    std::vector<bool> f(n, false);
+    std::queue<int> que;
+    que.push(s);
+    while (!que.empty()) {
+        int v = que.front();
+        que.pop();
+        for (auto nv : h[v]) {
+            f[nv] = true;
+        }
+        std::vector<int> L;
+        for (auto u : not_visit) {
+            if (f[u])
+                L.emplace_back(u);
+            else {
+                que.push(u);
+                func(v, u);
+            }
+        }
+        for (auto nv : h[v]) {
+            f[nv] = false;
+        }
+        not_visit = L;
+    }
+    return;
+}
+
+}  // namespace ebi
