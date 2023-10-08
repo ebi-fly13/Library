04667b6b6ede7c046ad938f68e3619ddeb062010
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-10-08 16:46:18 +0900

    add Multipoint Evaluation

---
+++ fps/multipoint_evaluation.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../fps/fps.hpp"
+
+namespace ebi {
+
+template <class mint, std::vector<mint> (*convolution)(
+                          const std::vector<mint> &, const std::vector<mint> &)>
+std::vector<mint> multipoint_evaluation(
+    const FormalPowerSeries<mint, convolution> &f, const std::vector<mint> &p) {
+    using FPS = FormalPowerSeries<mint, convolution>;
+    int m = 1;
+    while (m < (int)p.size()) m <<= 1;
+    std::vector<FPS> subproduct_tree(2 * m, {1});
+    for (int i = 0; i < (int)p.size(); i++) {
+        subproduct_tree[i + m] = FPS{-p[i], 1};
+    }
+    for (int i = m - 1; i >= 1; i--) {
+        subproduct_tree[i] =
+            subproduct_tree[2 * i] * subproduct_tree[2 * i + 1];
+    }
+    std::vector<FPS> subremainder_tree(2 * m);
+    subremainder_tree[1] = f % subproduct_tree[1];
+    for (int i = 2; i < m + (int)p.size(); i++) {
+        if (subremainder_tree[i / 2].empty()) continue;
+        subremainder_tree[i] = subremainder_tree[i / 2] % subproduct_tree[i];
+    }
+    std::vector<mint> fp(p.size());
+    for (int i = 0; i < (int)p.size(); i++) {
+        if (subremainder_tree[i + m].empty())
+            fp[i] = 0;
+        else
+            fp[i] = subremainder_tree[i + m][0];
+    }
+    return fp;
+}
+
+}  // namespace ebi
---
+++ test/polynomial/Multipoint_Evaluation.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/multipoint_evaluation"
+
+#include "../../fps/multipoint_evaluation.hpp"
+
+#include "../../convolution/ntt.hpp"
+#include "../../fps/fps.hpp"
+#include "../../template/template.hpp"
+#include "../../utility/modint.hpp"
+
+namespace ebi {
+
+using mint = modint998244353;
+using FPS = FormalPowerSeries<mint, convolution>;
+
+void main_() {
+    int n, m;
+    std::cin >> n >> m;
+    FPS f(n);
+    std::cin >> f;
+    std::vector<mint> p(m);
+    std::cin >> p;
+    auto fp = multipoint_evaluation<mint, convolution>(f, p);
+    std::cout << fp << '\n';
+}
+
+}  // namespace ebi
+
+int main() {
+    ebi::fast_io();
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        ebi::main_();
+    }
+    return 0;
+}
