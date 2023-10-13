67990ec94fc22206dafa2c22ef25ecaf386a3498
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-10-13 14:18:34 +0900

    add Polynomial Interporation

---
+++ fps/polynomial_interpolation.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../fps/fps.hpp"
+#include "../fps/multipoint_evaluation.hpp"
+
+namespace ebi {
+
+template <class mint, std::vector<mint> (*convolution)(
+                          const std::vector<mint> &, const std::vector<mint> &)>
+FormalPowerSeries<mint, convolution> polynomial_interpolation(
+    const std::vector<mint> &xs, const std::vector<mint> &ys) {
+    using FPS = FormalPowerSeries<mint, convolution>;
+    assert(xs.size() == ys.size());
+    int m = 1;
+    int n = xs.size();
+    while (m < n) m <<= 1;
+    std::vector<FPS> subproduct_tree(2 * m, {1});
+    for (int i = 0; i < (int)xs.size(); i++) {
+        subproduct_tree[i + m] = FPS{-xs[i], 1};
+    }
+    for (int i = m - 1; i >= 1; i--) {
+        subproduct_tree[i] =
+            subproduct_tree[2 * i] * subproduct_tree[2 * i + 1];
+    }
+    std::vector<mint> fp =
+        multipoint_evaluation(subproduct_tree[1].differential(), xs);
+    std::vector<FPS> f(2 * m);
+    for (int i = 0; i < n; i++) {
+        f[i + m] = FPS{ys[i] / fp[i]};
+    }
+    for (int i = m - 1; i >= 1; i--) {
+        f[i] = f[2 * i] * subproduct_tree[2 * i + 1] +
+               subproduct_tree[2 * i] * f[2 * i + 1];
+    }
+    f[1].resize(n);
+    return f[1];
+}
+
+}  // namespace ebi
---
+++ test/polynomial/Polynomial_Interpolation.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/polynomial_interpolation"
+
+#include "fps/polynomial_interpolation.hpp"
+
+#include "../../template/template.hpp"
+#include "convolution/ntt.hpp"
+#include "utility/modint.hpp"
+
+namespace ebi {
+
+using mint = modint998244353;
+
+void main_() {
+    int n;
+    std::cin >> n;
+    std::vector<mint> x(n), y(n);
+    std::cin >> x >> y;
+    auto f = polynomial_interpolation<mint, convolution>(x, y);
+    rep(i, 0, n) {
+        std::cout << f[i] << " \n"[i == n - 1];
+    }
+}
+
+}  // namespace ebi
+
+int main() {
+    ebi::fast_io();
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        ebi::main_();
+    }
+    return 0;
+}
