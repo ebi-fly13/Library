4ab43d1e2e6dc11bca054edb675b94a2cb020912
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-11-15 02:27:24 +0900

    add test about slope trick

---
+++ test/yuki/yuki_1077.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1077"
+
+#include "../../template/template.hpp"
+#include "../../data_structure/slope_trick.hpp"
+
+namespace ebi {
+
+void main_() {
+    int n;
+    std::cin >> n;
+    slope_trick<i64> st;
+    rep(i,0,n) {
+        i64 y;
+        std::cin >> y;
+        st.left_cumulative_min();
+        st.add_abs(y);
+    }
+    std::cout << st.min() << '\n';
+}
+
+}  // namespace ebi
+
+int main() {
+    ebi::fast_io();
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        ebi::main_();
+    }
+    return 0;
+}
