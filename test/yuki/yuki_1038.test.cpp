75f7a796763ffc3844366f4a5cf36596df1fada1
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2024-01-05 02:16:46 +0900

    add test

---
+++ test/yuki/yuki_1038.test.cpp
@@ -0,0 +1,60 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1038"
+
+#include "../../data_structure/dual_segtree.hpp"
+#include "../../graph/template.hpp"
+#include "../../template/template.hpp"
+#include "../../tree/contour_query_on_tree.hpp"
+
+namespace ebi {
+
+i64 op(i64 a, i64 b) {
+    return a + b;
+}
+
+i64 e() {
+    return 0;
+}
+
+void main_() {
+    int n, q;
+    std::cin >> n >> q;
+    graph g(n);
+    rep(i, 0, n - 1) {
+        int a, b;
+        std::cin >> a >> b;
+        a--;
+        b--;
+        g.add_edge(a, b);
+    }
+    std::vector<i64> res(n, 0);
+    contour_query_on_tree cq(g);
+    dual_segtree<i64, op, e> seg(cq.size());
+    while (q--) {
+        int x, y;
+        i64 z;
+        std::cin >> x >> y >> z;
+        x--;
+        i64 ans = res[x];
+        for (auto i : cq.get_vertex(x)) {
+            ans += seg.get(i);
+        }
+        std::cout << ans << '\n';
+        y++;
+        res[x] += z;
+        for (auto [l, r] : cq.get_contour_from_vertex(x, 0, y)) {
+            seg.apply(l, r, z);
+        }
+    }
+}
+
+}  // namespace ebi
+
+int main() {
+    ebi::fast_io();
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        ebi::main_();
+    }
+    return 0;
+}
---
+++ test/yuki/yuki_1796.test.cpp
@@ -0,0 +1,75 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1796"
+
+#include "../../convolution/ntt.hpp"
+#include "../../fps/middle_product.hpp"
+#include "../../graph/template.hpp"
+#include "../../math/binomial.hpp"
+#include "../../modint/modint.hpp"
+#include "../../template/template.hpp"
+#include "../../tree/centroid_decomposition.hpp"
+
+namespace ebi {
+
+using mint = modint998244353;
+
+void main_() {
+    Binomial<mint> binom(300000);
+    int n;
+    std::cin >> n;
+    std::vector<mint> q(n);
+    std::cin >> q;
+    graph tree(n);
+    auto ans = q;
+    rep(i, 0, n - 1) {
+        int u, v;
+        std::cin >> u >> v;
+        u--;
+        v--;
+        tree.add_edge(u, v);
+        ans[u] += q[v] * binom.inv(4);
+        ans[v] += q[u] * binom.inv(4);
+    }
+    auto calc = [&](const std::vector<int> &par, const std::vector<int> &vs,
+                    int n0, int n1) {
+        int sz = (int)par.size();
+        std::vector<int> depth(sz, 0);
+        rep(i, 1, sz) {
+            depth[i] = depth[par[i]] + 1;
+        }
+        auto calc2 = [&](int l0, int r0, int l1, int r1) -> void {
+            int sz0 = *std::max_element(depth.begin() + l0, depth.begin() + r0);
+            int sz1 = *std::max_element(depth.begin() + l1, depth.begin() + r1);
+            std::vector<mint> f(sz0 + sz1 + 1), g(sz1 + 1);
+            rep(i, 0, f.size()) {
+                f[i] = binom.inv(i + 1) * binom.inv(i + 1);
+            }
+            rep(i, l1, r1) {
+                g[depth[i]] += q[vs[i]];
+            }
+            auto h = middle_product<mint, convolution>(f, g);
+            assert((int)h.size() == sz0 + 1);
+            rep(i, l0, r0) {
+                ans[vs[i]] += h[depth[i]];
+            }
+        };
+        calc2(1, 1 + n0, 1 + n0, 1 + n0 + n1);
+        calc2(1 + n0, 1 + n0 + n1, 1, 1 + n0);
+    };
+    centroid_decomposition<1>(tree, calc);
+    rep(i, 0, n) {
+        ans[i] *= binom.f(n) * binom.f(n);
+        std::cout << ans[i] << '\n';
+    }
+}
+
+}  // namespace ebi
+
+int main() {
+    ebi::fast_io();
+    int t = 1;
+    // std::cin >> t;
+    while (t--) {
+        ebi::main_();
+    }
+    return 0;
+}
