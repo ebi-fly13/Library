b58d50ae291629c7124ad8a26959935be1cd2455
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2024-05-24 18:44:15 +0900

    fix middle product

--- fps/middle_product.hpp
+++ fps/middle_product.hpp
@@ -7,9 +7,25 @@
 #include <vector>
 
 #include "../convolution/ntt.hpp"
+#include "../fps/fps.hpp"
 #include "../modint/base.hpp"
 
 namespace ebi {
+
+template <class T>
+std::vector<T> middle_product_naive(const std::vector<T> &a,
+                                    const std::vector<T> &b) {
+    int n = (int)a.size();
+    int m = (int)b.size();
+    assert(n >= m);
+    std::vector<T> c(n - m + 1, 0);
+    for (int i : std::views::iota(0, n - m + 1)) {
+        for (int j : std::views::iota(0, m)) {
+            c[i] += b[j] * a[i + j];
+        }
+    }
+    return c;
+}
 
 template <Modint mint>
 std::vector<mint> middle_product(const std::vector<mint> &a,
@@ -37,19 +53,28 @@ std::vector<mint> middle_product(const std::vector<mint> &a,
     return fa;
 }
 
-template <class T>
-std::vector<T> middle_product_naive(const std::vector<T> &a,
-                                    const std::vector<T> &b) {
-    int n = (int)a.size();
-    int m = (int)b.size();
-    assert(n >= m);
-    std::vector<T> c(n - m + 1, 0);
-    for (int i : std::views::iota(0, n - m + 1)) {
-        for (int j : std::views::iota(0, m)) {
-            c[i] += b[j] * a[i + j];
-        }
+template <Modint mint>
+FormalPowerSeries<mint> middle_product(const FormalPowerSeries<mint> &a,
+                                       const FormalPowerSeries<mint> &b) {
+    using FPS = FormalPowerSeries<mint>;
+    assert(a.size() >= b.size());
+    if (std::min(a.size() - b.size() + 1, b.size()) <= 60) {
+        return middle_product_naive<mint>(a, b);
     }
-    return c;
+    int n = std::bit_ceil(a.size());
+    FPS fa(n), fb(n);
+    std::copy(a.begin(), a.end(), fa.begin());
+    std::copy(b.rbegin(), b.rend(), fb.begin());
+    fa.fft();
+    fb.fft();
+    for (int i = 0; i < n; i++) {
+        fa[i] *= fb[i];
+    }
+    fa.ifft();
+    fa /= n;
+    fa = fa.pre(a.size());
+    fa.erase(fa.begin(), fa.begin() + b.size() - 1);
+    return fa;
 }
 
 }  // namespace ebi
