c987df3eaa5e806016bfe97ab6c5b1aea8238603
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2024-06-25 15:37:23 +0900

    fix bm

--- fps/berlekamp_massey.hpp
+++ fps/berlekamp_massey.hpp
@@ -12,14 +12,6 @@ std::vector<mint> berlekamp_massey(const std::vector<mint> &s) {
     std::vector<mint> C = {1}, B = {1};
     int L = 0, m = 1;
     mint b = 1;
-    auto proceed = [](std::vector<mint> &C, const std::vector<mint> &B, mint d,
-                      mint b, int m) -> void {
-        C.resize(std::max(C.size(), B.size() + m));
-        mint f = d / b;
-        for (int i = 0; i < (int)B.size(); i++) {
-            C[i + m] -= f * B[i];
-        }
-    };
     for (int n = 0; n < (int)s.size(); n++) {
         mint d = s[n];
         for (int i = 1; i <= L; i++) {
@@ -29,13 +21,21 @@ std::vector<mint> berlekamp_massey(const std::vector<mint> &s) {
             m++;
         } else if (2 * L <= n) {
             auto T = C;
-            proceed(C, B, d, b, m);
+            mint f = d / b;
+            C.resize((int)B.size() + m);
+            for (int i = 0; i < (int)B.size(); i++) {
+                C[i + m] -= f * B[i];
+            }
             L = n + 1 - L;
             B = T;
             b = d;
             m = 1;
         } else {
-            proceed(C, B, d, b, m++);
+            mint f = d / b;
+            for (int i = 0; i < (int)B.size(); i++) {
+                C[i + m] -= f * B[i];
+            }
+            m++;
         }
     }
     return C;
